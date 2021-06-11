ce4678d55cd3ff58015a003d5175f437d101ca62
rchry001 <84558191+rchry001@users.noreply.github.com>
2021-06-10 23:32:17 -0400

    Added User input choice to select stock

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -11,7 +11,18 @@ ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 # make a request
 
-symbol = "VYM" # ask for a user input
+symbol = "VYM" # ask for a user input....Eventually could add a user input for risk tolerance and investment time horizon
+
+# Information input requirement: Set up user choice to make user pick a stock
+
+user_choice = input("Please enter a ticker symbol, or multiple symbols, to view information:")
+print("TICKER SYMBOL SELECTED: ", user_choice)
+
+
+#
+# 
+# Validation requirement: to prevent an error if stock ticker symbol is likely invalid
+# Logic: If what is entered is > than 4 characters then try again, if < than 1 character try again
 
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
 
