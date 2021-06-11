b292f1678074a7dafea6daeff5e63cea4671e52a
rchry001 <84558191+rchry001@users.noreply.github.com>
2021-06-10 23:12:08 -0400

    Setting up the Functionality Requirements

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,18 +1,22 @@
 # this is the "app/robo_advisor.py" file
 
-print("-------------------------")
-print("SELECTED SYMBOL: IBM")
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
-print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv() # go get env vars from the .env file
+
+# read env variables
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+# make a request
+
+symbol = "VYM" # ask for a user input
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+
+# response is a command used as part of the requests package
+
+response = requests.get(request_url)
+print(type(response))
+print(response.text)
